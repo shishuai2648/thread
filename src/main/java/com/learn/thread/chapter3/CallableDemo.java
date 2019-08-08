4d763d85b49b7bfa83da86798dae8931fa088259
shishuai2648@163.com <a8215899>
2019-08-08 18:02:57 +0800

    项目初始

---
+++ src/main/java/com/learn/thread/chapter3/CallableDemo.java
@@ -0,0 +1,26 @@
+package com.learn.thread.chapter3;
+
+import java.util.concurrent.*;
+
+public class CallableDemo implements Callable<String> {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        CallableDemo callableDemo = new CallableDemo();
+
+        Future<String> future = executorService.submit(callableDemo);
+        String s = future.get();
+
+        System.out.println(s);
+        executorService.shutdown();
+
+
+    }
+
+
+    @Override
+    public String call() throws Exception {
+        return "string" +1;
+    }
+}
